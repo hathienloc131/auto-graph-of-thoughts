8cf9775c881d5164458575278d4936ade62c78c7
htrbao <trbaohoo@gmail.com>
2023-11-18 12:12:40 +0700

    [baoht] drawer

---
+++ graph_of_thoughts/prompter/prompter.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2023 ETH Zurich.
+#                    All rights reserved.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# main authors: Robert Gerstenberger, Nils Blach
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+
+class Prompter(ABC):
+    """
+    Abstract base class that defines the interface for all prompters.
+    Prompters are used to generate the prompts for the language models.
+    """
+
+    @abstractmethod
+    def aggregation_prompt(self, state_dicts: List[Dict], **kwargs) -> str:
+        """
+        Generate a aggregation prompt for the language model.
+        """
+        pass
+
+    @abstractmethod
+    def improve_prompt(self, **kwargs) -> str:
+        """
+        Generate an improve prompt for the language model.
+        The thought state is unpacked to allow for additional keyword arguments
+        and concrete implementations to specify required arguments explicitly.
+        """
+        pass
+
+    @abstractmethod
+    def generate_prompt(self, num_branches: int, **kwargs) -> str:
+        """
+        Generate a generate prompt for the language model.
+        The thought state is unpacked to allow for additional keyword arguments
+        and concrete implementations to specify required arguments explicitly.
+        """
+        pass
+
+    @abstractmethod
+    def split_prompt(self, **kwargs) -> str:
+        """
+        Generate a split prompt for the language model.
+        The thought state is unpacked to allow for additional keyword arguments
+        and concrete implementations to specify required arguments explicitly.
+        """
+        pass
